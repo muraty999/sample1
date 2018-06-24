0594a30e77bb50afe1b474d0f5b4c5bd71c86d58
murat yılmaz <muraty999@gmail.com>
2018-06-24 18:16:46 +0300

    Depency feature

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,5 +1,7 @@
 package clinic.programming.training;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
 	public static void main(String [] args) {
 		System.out.println("Hello world !");
